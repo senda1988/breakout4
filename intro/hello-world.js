9711dfa3710473f56d6c49fe82cc1ea65e7af7d6
LukasProgress <probst404@gmail.com>
2025-03-03 10:59:13 +0100

    Python.py and hello-world.js

---
+++ intro/hello-world.js
@@ -0,0 +1,18 @@
+//---------------------+
+//                     |
+//      JAVASCRIPT     |
+//                     |
+//---------------------+ 
+
+// Kommentare mit zwei Schrägstrichen
+
+// Hello World!
+console.log("Hello world!");
+// Im Browser geht auch alert("Hello world")
+
+// Variablen Deklarieren ("Erstellen") (Mit let oder var) (const für Konstanten)
+let my_number = 1
+
+// Kleine Fingerübung: 
+// Deklariere eine variable name und eine Variable alter
+// Gebt beide auf der Konsole aus 
---
+++ intro/python.py
@@ -0,0 +1,40 @@
+#----------------------+
+#                      |
+#        PYTHON        |
+#                      |
+#----------------------+ 
+
+# Kommentare mit einer Raute
+
+# Hello World!
+print("Hello world!")
+
+
+# Variablen Deklarieren ("Erstellen")
+my_number = 1
+
+
+# Primitive Datentypen in Python: 
+
+
+# Primitive Rechenoperationen in Python:
+
+
+# Boolsche Operationen in Python
+
+
+# Fallunterscheidungen in Python
+
+
+# Listen in Python:
+
+
+# Dictionaries in Python: 
+
+
+# Funktionen in Python:
+
+
+# Loops in Python: 
+
+
